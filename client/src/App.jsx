8783c2a8094b554cf184d090094eee724f1924f7
Mohammed Imran <mohdimran97057@gmail.com>
2024-03-21 10:49:10 +0530

    github api integrated.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 import HomePage from "./pages/HomePage";
 import Signup from "./pages/Signup";
@@ -6,21 +7,22 @@ import ExplorePage from "./pages/ExplorePage";
 import Login from "./pages/Login";
 import { LikesPage } from "./pages/LikesPage";
 
-import Sidebar from './components/Sidebar';
+import Sidebar from "./components/Sidebar";
 function App() {
   return (
-   <div className="flex text-white">
-    <Sidebar />
-    <div className="max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/explore" element={<ExplorePage />} />
-        <Route path="/likes" element={<LikesPage />} />
-      </Routes>
+    <div className="flex text-white">
+      <Sidebar />
+      <div className="max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/likes" element={<LikesPage />} />
+        </Routes>
+        <Toaster />
+      </div>
     </div>
-   </div>
   );
 }
 
